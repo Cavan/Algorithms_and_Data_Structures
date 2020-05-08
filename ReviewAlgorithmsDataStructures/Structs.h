70d40e51f0f5caa9fa636244b3e499d279ca0882
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2020-05-08 19:33:00 -0400

    Add project files.

---
+++ ReviewAlgorithmsDataStructures/Structs.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class Structures {
+
+	struct Rectangle
+	{
+		int length;
+		int width;
+	};
+
+public:
+	void CreateStruct() {
+		struct Rectangle r;
+
+		struct Rectangle r = { 10,5 };
+		
+
+		
+
+	}
+
+};
