4b20c060568742e56189bb1fbf84153988503538
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2020-05-19 08:58:02 -0400

    Reviewed tree recursion

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -18,20 +18,45 @@ void fun1(int n) {		// T(n)
 // Time complexity O(n)
 
 void fun2(int n) {
+	
 	if (n > 0) {
 		printf("%d ", n);
 		fun2(n - 1);
 	}
 }
 
+// Static and Global Recursion
+int fun3(int n)
+{
+	printf("\n");
+	static int x = 0;
+	if (n > 0) {
+		x++;
+		return fun3(n - 1) + x;
+	}
+}
+
+
+// Tree Recursion
+// Time Complexity is O(2^n)
+// Space Complexity is O(n)
+void treeRecursion(int n) {
+	if (n > 0) {
+		printf("%d ", n);
+		treeRecursion(n - 1);
+		treeRecursion(n - 1);
+	}
+}
+
+// Indirect Recursion
+
+
 
 
 int main() {
 	
-	int x = 3;
-	fun1(x);
-	printf("\n");
-	fun2(x);
+	 //Tree Recursion
+	treeRecursion(3);
 
 	return 0;
 }
