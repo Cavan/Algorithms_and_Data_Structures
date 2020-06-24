6c8348c8fd325e7b343e8525d58e6474bf032169
Cavan Biggs <cbiggs7635@conestogac.on.ca>
2020-06-23 20:18:42 -0400

    Completed Linear & Binary ADT search

--- ArrayADT/ArrayADT.cpp
+++ ArrayADT/ArrayADT.cpp
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning(disable: 4996)
+
+using namespace std;
+
 struct Array
 {
-	int* A;
+	int A[10];
 	int size;
 	int length;
 };
 
+// Display the array
 void Display(struct Array arr) {
 	int i;
 	printf("Elements are \n");
@@ -16,24 +20,124 @@ void Display(struct Array arr) {
 	}
 }
 
+// Swap method
+void swap(int* x, int* y) {
+	int temp = *x;
+		*x = *y;
+		*y = temp;
+}
+
+//Append to the end of the array
+void Append(struct Array* arr, int x) {
+	if (arr->length < arr->size)
+	{
+		arr->A[arr->length++] = x;
+	}
+}
+
+//Insert element into array
+void Insert(struct Array* arr, int index, int x) 
+{
+	int i;
+	if (index > 0 && index <= arr->length) 
+	{
+		for (i = arr->length; i > index; i--)
+		{
+			arr->A[i] = arr->A[i - 1];
+		}
+		arr->A[index] = x;
+		arr->length++;
+	}
+
+}
+
+// Delete an element from a given index
+int Delete(struct Array* arr, int index) {
+	int x = 0;
+	int i;
+	if (index >= 0 && index < arr->length) {
+		x = arr->A[index];
+		for (i = index; i < arr->length - 1; i++)
+		{
+			arr->A[i] = arr->A[i + 1];
+		}
+		arr->length--;
+		return x;
+	}
+	return 0;
+}
+
+// Linear Search
+int LinearSearch(struct Array* arr, int key) {
+	int i;
+	for (i = 0; i < arr->length; i++) {
+		if (key == arr->A[i])
+		{
+			swap(&arr->A[i], &arr->A[0]);
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+// Binary Search
+
+int BinarySearch(struct Array arr, int key) {
+	int l, mid, h;
+	l = 0;
+	h = arr.length - 1;
+	while (l <= h)
+	{
+		mid = (l + h) / 2;
+		if (key == arr.A[mid])
+		{
+			return mid;
+		}
+		else if(key<arr.A[mid])
+		{
+			h = mid - 1;
+		}
+		else
+		{
+			l = mid + 1;
+		}
+	}
+	return -1;
+}
+
+// Recursive
+int RBinSearch(int a[], int l, int h, int key)
+{
+	int mid = 0;
+	if (l <= h)
+	{
+		mid = (l + h) / 2;
+		if (key == a[mid])
+		{
+			return mid;
+		}
+		else if (key < a[mid])
+		{
+			return RBinSearch(a, l, mid - 1, key);
+		}
+		else
+		{
+			return RBinSearch(a, mid + 1, h, key);
+		}
+		return -1;
+	}
+}
+
+
+
+
+
 int main() {
 
-	struct Array arr;
-	int n, i;
-	printf("Enter size of an array");
-	scanf("%d", &arr.size);
-	arr.A = (int*)malloc(arr.size * sizeof(int));
-	arr.length = 0;
-
-	printf("Enter number of numbers");
-	scanf("%d", &n);
-
-	printf("Enter all Elements\n");
-	for (i = 0; i < n; i++) {
-		scanf("%d", &arr.A[i]);
-	}
-	arr.length = n;
-	Display(arr);
+	struct Array arr1 = { {2,3,9,16,18,21,28,32,35},10,9 };
+	printf("\n\n%d\n\n", BinarySearch(arr1, 16));
+	Display(arr1);
 
 
 	return 0;
